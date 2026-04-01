186afdbe476c43554b03f6796afd6f34115ec90a
autorun <20293349@qq.com>
2026-04-01 19:04:53 +0800

    init project

---
+++ src/main/java/ai/restosuite/infrastructure/operation/App.java
@@ -0,0 +1,13 @@
+package ai.restosuite.infrastructure.operation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
